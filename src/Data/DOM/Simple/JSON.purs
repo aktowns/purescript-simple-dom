5ac0716d58d0ed11ae49d6ecc686311dd0570686
Ashley Towns <ashleyis@me.com>
2014-08-06 00:24:21 +1000

    oops

---
+++ src/Data/DOM/Simple/JSON.purs
@@ -0,0 +1,21 @@
+module Data.DOM.Simple.JSON where
+
+import Control.Monad.Eff
+import Data.Dom.Simple.Element
+
+foreign import data JSONObject :: *
+
+
+foreign import stringify
+  "function stringify(obj) {        \
+  \  return function () {           \
+  \    return JSON.stringify(obj);  \
+  \  };                             \
+  \}" :: forall eff a. a -> (Eff (dom :: DOM | eff) String)
+
+foreign import parse
+  "function getDocument(str) { \
+  \  return function () {      \
+  \    return JSON.parse(str); \
+  \  };                        \
+  \}" :: forall eff. String -> (Eff (dom :: DOM | eff) JSONObject)
